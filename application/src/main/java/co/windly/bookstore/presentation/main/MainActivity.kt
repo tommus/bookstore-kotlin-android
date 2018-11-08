fe9316bd32a7b3d022e5334dcdc9b35d832f7255
Tomasz Dzieniak <tomasz.dzieniak@windly.co>
2018-11-08 14:02:28 +0100

    Refactor onBackPressed() in main activity.

--- application/src/main/java/co/windly/bookstore/presentation/main/MainActivity.kt
+++ application/src/main/java/co/windly/bookstore/presentation/main/MainActivity.kt
@@ -72,26 +72,21 @@ class MainActivity : AppCompatActivity(), Drawer.OnDrawerItemClickListener {
       // In case if drawer is open - close it.
       if (it.isDrawerOpen) {
         it.closeDrawer()
-      } else {
+      }
 
-          with(findNavController(this, R.id.mainHostFragment)) {
+      // Otherwise ask for exit confirmation.
+      else with(findNavController(this, R.id.mainHostFragment)) {
 
-              // Retrieve start destination id.
-              val homeDestinationIdentifier = graph.startDestination
+        when (graph.startDestination) {
 
-              // Retrieve current destination id.
-              val currentDestinationIdentifier = currentDestination?.id
-
-              // When we've got home destination id then show exit confirmation dialog - otherwise just follow the orders of supertype.
-              when (homeDestinationIdentifier) {
-                  currentDestinationIdentifier -> {
-
-                      // Show confirmation dialog.
-                      showConfirmExitDialog()
-                  }
-                  else -> super.onBackPressed()
-              }
+          // Show confirmation dialog.
+          currentDestination?.id -> {
+            showConfirmExitDialog()
           }
+
+          // Continue with default behavior.
+          else -> super.onBackPressed()
+        }
       }
     }
   }
@@ -106,85 +101,85 @@ class MainActivity : AppCompatActivity(), Drawer.OnDrawerItemClickListener {
 
     // Return drawer.
     return DrawerBuilder()
-        .withActivity(this)
-        .withToolbar(toolbar)
-        .withHasStableIds(true)
-        .withAccountHeader(accountHeader!!)
-        .addDrawerItems(*prepareDrawerItems())
-        .addStickyDrawerItems(*prepareStickyDrawerItems())
-        .withSavedInstance(savedInstanceState)
-        .withShowDrawerOnFirstLaunch(true)
-        .build()
+      .withActivity(this)
+      .withToolbar(toolbar)
+      .withHasStableIds(true)
+      .withAccountHeader(accountHeader!!)
+      .addDrawerItems(*prepareDrawerItems())
+      .addStickyDrawerItems(*prepareStickyDrawerItems())
+      .withSavedInstance(savedInstanceState)
+      .withShowDrawerOnFirstLaunch(true)
+      .build()
   }
 
   private fun materialize(item: AbstractBadgeableDrawerItem<*>): AbstractBadgeableDrawerItem<*> {
 
     // Materialize item.
     return item
-        .withTextColor(ContextCompat.getColor(this, R.color.black))
-        .withIconColor(ContextCompat.getColor(this, R.color.black))
-        .withSelectedTextColor(ContextCompat.getColor(this, R.color.colorPrimary))
-        .withSelectedIconColor(ContextCompat.getColor(this, R.color.colorPrimary))
-        .withDisabledTextColor(ContextCompat.getColor(this, R.color.black))
+      .withTextColor(ContextCompat.getColor(this, R.color.black))
+      .withIconColor(ContextCompat.getColor(this, R.color.black))
+      .withSelectedTextColor(ContextCompat.getColor(this, R.color.colorPrimary))
+      .withSelectedIconColor(ContextCompat.getColor(this, R.color.colorPrimary))
+      .withDisabledTextColor(ContextCompat.getColor(this, R.color.black))
   }
 
   private fun prepareDrawerItems(): Array<IDrawerItem<*, *>> {
     return arrayOf(
 
-        // Prepare home item.
-        materialize(
-            PrimaryDrawerItem()
-                .withName(R.string.drawer_item_home)
-                .withIcon(R.drawable.ic_home)
-                .withIconTintingEnabled(true)
-                .withIdentifier(MainMenuItem.HOME)
-        ),
+      // Prepare home item.
+      materialize(
+        PrimaryDrawerItem()
+          .withName(R.string.drawer_item_home)
+          .withIcon(R.drawable.ic_home)
+          .withIconTintingEnabled(true)
+          .withIdentifier(MainMenuItem.HOME)
+      ),
 
-        // Prepare books item.
-        materialize(
-            PrimaryDrawerItem()
-                .withName(R.string.drawer_item_book_list)
-                .withIcon(R.drawable.ic_books)
-                .withIconTintingEnabled(true)
-                .withIdentifier(MainMenuItem.BOOKS)
-        ),
+      // Prepare books item.
+      materialize(
+        PrimaryDrawerItem()
+          .withName(R.string.drawer_item_book_list)
+          .withIcon(R.drawable.ic_books)
+          .withIconTintingEnabled(true)
+          .withIdentifier(MainMenuItem.BOOKS)
+      ),
 
-        // Prepare basket item with badge.
-        materialize(
-            PrimaryDrawerItem()
-                .withName(R.string.drawer_item_cart)
-                .withIcon(R.drawable.ic_basket)
-                .withIconTintingEnabled(true)
-                .withIdentifier(MainMenuItem.BASKET)
-                .withBadgeStyle(
-                    BadgeStyle()
-                        .withTextColor(Color.WHITE)
-                        .withColorRes(R.color.colorAccent)
-                )
-        )
+      // Prepare basket item with badge.
+      materialize(
+        PrimaryDrawerItem()
+          .withName(R.string.drawer_item_cart)
+          .withIcon(R.drawable.ic_basket)
+          .withIconTintingEnabled(true)
+          .withIdentifier(MainMenuItem.BASKET)
+          .withBadgeStyle(
+            BadgeStyle()
+              .withTextColor(Color.WHITE)
+              .withColorRes(R.color.colorAccent)
+          )
+      )
     )
   }
 
   private fun prepareStickyDrawerItems(): Array<IDrawerItem<*, *>> {
     return arrayOf(
 
-        // Prepare settings item.
-        materialize(
-            SecondaryDrawerItem()
-                .withName(R.string.drawer_item_settings)
-                .withIcon(R.drawable.ic_settings)
-                .withIconTintingEnabled(true)
-                .withIdentifier(MainMenuItem.SETTINGS)
-        ),
+      // Prepare settings item.
+      materialize(
+        SecondaryDrawerItem()
+          .withName(R.string.drawer_item_settings)
+          .withIcon(R.drawable.ic_settings)
+          .withIconTintingEnabled(true)
+          .withIdentifier(MainMenuItem.SETTINGS)
+      ),
 
-        // Prepare about item.
-        materialize(
-            SecondaryDrawerItem()
-                .withName(R.string.drawer_item_about)
-                .withIcon(R.drawable.ic_about)
-                .withIconTintingEnabled(true)
-                .withIdentifier(MainMenuItem.ABOUT)
-        )
+      // Prepare about item.
+      materialize(
+        SecondaryDrawerItem()
+          .withName(R.string.drawer_item_about)
+          .withIcon(R.drawable.ic_about)
+          .withIconTintingEnabled(true)
+          .withIdentifier(MainMenuItem.ABOUT)
+      )
     )
   }
 
@@ -221,24 +216,24 @@ class MainActivity : AppCompatActivity(), Drawer.OnDrawerItemClickListener {
 
     // Return account header.
     return AccountHeaderBuilder()
-        .withActivity(this)
-        .withCompactStyle(false)
-        .withProfileImagesClickable(false)
-        .withSelectionListEnabledForSingleProfile(false)
-        .addProfiles(*prepareProfiles(username, email))
-        .withSavedInstance(savedInstanceState)
-        .build()
+      .withActivity(this)
+      .withCompactStyle(false)
+      .withProfileImagesClickable(false)
+      .withSelectionListEnabledForSingleProfile(false)
+      .addProfiles(*prepareProfiles(username, email))
+      .withSavedInstance(savedInstanceState)
+      .build()
   }
 
   private fun prepareProfiles(name: String?, email: String?): Array<IProfile<*>> {
 
     // Prepare profile item.
     return arrayOf(
-        ProfileDrawerItem()
-            .withName(name)
-            .withEmail(email)
-            .withIcon(R.drawable.ic_avatar_male)
-            .withIdentifier(MainMenuItem.PROFILE)
+      ProfileDrawerItem()
+        .withName(name)
+        .withEmail(email)
+        .withIcon(R.drawable.ic_avatar_male)
+        .withIdentifier(MainMenuItem.PROFILE)
     )
   }
 
@@ -250,16 +245,16 @@ class MainActivity : AppCompatActivity(), Drawer.OnDrawerItemClickListener {
 
     // Create nav options.
     return NavOptions
-        .Builder()
-        .setPopUpTo(R.id.home, inclusive)
-        .build()
+      .Builder()
+      .setPopUpTo(R.id.home, inclusive)
+      .build()
   }
 
   private fun navigateToDestination(@IdRes destinationId: Int, inclusive: Boolean) {
 
     // Find controller and navigate to destination using nav options.
     findNavController(this, R.id.mainHostFragment)
-        .navigate(destinationId, null, navOptions(inclusive))
+      .navigate(destinationId, null, navOptions(inclusive))
   }
 
   private fun navigateToHomeView() {
@@ -300,11 +295,11 @@ class MainActivity : AppCompatActivity(), Drawer.OnDrawerItemClickListener {
 
     // Show confirmation exit dialog.
     MaterialDialog(this)
-        .message(R.string.home_dialog_confirm_logout)
-        .positiveButton(R.string.common_yes)
-        .negativeButton(R.string.common_no)
-        .positiveButton { closeApplication() }
-        .show()
+      .message(R.string.home_dialog_confirm_logout)
+      .positiveButton(R.string.common_yes)
+      .negativeButton(R.string.common_no)
+      .positiveButton { closeApplication() }
+      .show()
   }
 
   //endregion
